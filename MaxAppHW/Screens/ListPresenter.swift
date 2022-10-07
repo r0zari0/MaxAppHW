8412c57d88377d1bd25d4e440d58899eb1ad4a76
Maksym <maksymstovolos2002@gmail.com>
2022-10-07 13:12:42 -0400

    1

---
+++ MaxAppHW/Screens/ListPresenter.swift
@@ -0,0 +1,49 @@
+//
+//  ListPresenter.swift
+//  MaxAppHW
+//
+//  Created by Max Stovolos on 9/24/22.
+//
+
+import Foundation
+import UIKit
+
+protocol ListPresenterProtocol {
+    var user: [MyUser] { get set }
+    
+    func showNewInfoVC(vc: UIViewController, user: MyUser)
+}
+
+class ListPresenter: ListPresenterProtocol {
+    
+    private let navigator: NavigatorProtocol
+    
+    var listVCProtocol: ListVCProtocol!
+    
+    init(navigator: NavigatorProtocol) {
+        self.navigator = navigator
+    }
+    
+    func set(listVCProtocol: ListVCProtocol) {
+        self.listVCProtocol = listVCProtocol
+    }
+    
+    var user: [MyUser] = [
+        .init(name: "Max", lastName: "Max"),
+        .init(name: "Dima", lastName: "Dima"),
+        .init(name: "Stas", lastName: "Stas"),
+        .init(name: "Tom", lastName: "Tom"),
+        .init(name: "Jon", lastName: "Jon")
+    ]
+    
+    func showNewInfoVC(vc: UIViewController, user: MyUser) {
+        navigator.showNewInfoVC(view: vc, delegate: self, user: user)
+    }
+}
+
+extension ListPresenter: NewInfoDelegate {
+    func sendInfo(with user: MyUser) {
+        self.user.append(user)
+        listVCProtocol.reloadTableView()
+    }
+}
